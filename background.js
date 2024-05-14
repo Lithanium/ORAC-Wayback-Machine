91343827a7877f0ac1b6f1f69d020a3a2db02e18
Lithanium <156085117+Lithanium@users.noreply.github.com>
2024-05-14 17:39:54 +1000

    Added console debugging

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
 // background.js
+
+console.log("Background script is running!");
+
+// Listen for messages from other scripts
+self.addEventListener('message', event => {
+    const message = event.data;
+    
+    if (message.type === 'consoleMessage') {
+      console.log('[From Content Script/Popup/Background Script]:', message.message);
+    }
+  });
+  
